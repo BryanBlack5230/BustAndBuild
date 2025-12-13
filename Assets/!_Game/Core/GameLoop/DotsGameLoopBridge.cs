8e11fc43aff66b85cb02e58e6e6290df2bcf5e53
BryanBlack5230 <bryanblack523@gmail.com>
2025-12-13 23:37:49 +0900

    cleaning logs

--- Assets/!_Game/Core/GameLoop/DotsGameLoopBridge.cs
+++ Assets/!_Game/Core/GameLoop/DotsGameLoopBridge.cs
@@ -1,15 +1,11 @@
 using System;
 using Unity.Entities;
-using UnityEngine;
 
 namespace GameManagement
 {
     [UpdateInGroup(typeof(SimulationSystemGroup))]
     [UpdateAfter(typeof(BeginSimulationEntityCommandBufferSystem))]
-    public partial class GameLoopSystemGroup : ComponentSystemGroup
-    {
-        // Note: This will be controlled externally via Enabled property
-    }
+    public partial class GameLoopSystemGroup : ComponentSystemGroup { }
     
     public class DotsGameLoopBridge : IGameStartListener, IGamePauseListener, IGameResumeListener, IGameFinishListener, IDisposable
     {
@@ -21,31 +17,18 @@ namespace GameManagement
         public void Initialize()
         {
             _world = World.DefaultGameObjectInjectionWorld;
-        
-            if (_world == null || !_world.IsCreated)
-            {
-                Debug.LogError("[DotsGameLoopBridge] No default world found! DOTS systems won't run.");
-                return;
-            }
+            if (_world == null || !_world.IsCreated) return;
         
             _gameLoopGroup = _world.GetExistingSystemManaged<GameLoopSystemGroup>();
         
-            if (_gameLoopGroup == null)
-            {
-                Debug.LogError("[DotsGameLoopBridge] GameLoopSystemGroup not found in world!");
-                return;
-            }
+            if (_gameLoopGroup == null) return;
         
             _gameLoopGroup.Enabled = false;
             _isInitialized = true;
-        
-            Debug.Log($"[DotsGameLoopBridge] Initialized. GameLoopSystemGroup: {_gameLoopGroup != null}");
         }
         public void OnStartGame()
         {
             if (!_isInitialized) return;
-        
-            Debug.Log("[DotsGameLoopBridge] Starting DOTS systems");
         
             _gameLoopGroup.Enabled = true;
         }
@@ -54,16 +37,12 @@ namespace GameManagement
         {
             if (!_isInitialized) return;
         
-            Debug.Log("[DotsGameLoopBridge] Pausing DOTS systems");
-        
             _gameLoopGroup.Enabled = false;
         }
 
         public void OnResume()
         {
             if (!_isInitialized) return;
-        
-            Debug.Log("[DotsGameLoopBridge] Resuming DOTS systems");
         
             _gameLoopGroup.Enabled = true;
         }
@@ -72,8 +51,6 @@ namespace GameManagement
         {
             if (!_isInitialized) return;
         
-            Debug.Log("[DotsGameLoopBridge] Finishing DOTS systems");
-        
             _gameLoopGroup.Enabled = false;
         }
 
@@ -81,12 +58,7 @@ namespace GameManagement
         {
             if (!_isInitialized) return;
             
-            Debug.Log("[DotsGameLoopBridge] Disposing");
-            
-            if (_gameLoopGroup != null)
-            {
-                _gameLoopGroup.Enabled = false;
-            }
+            _gameLoopGroup.Enabled = false;
         }
     }
 }
